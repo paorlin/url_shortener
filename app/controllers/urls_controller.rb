1ec00a26cb6dd463b047be842f82e0563676820a
Patrick Ortmann <patrick.ortmann07@gmail.com>
2018-03-10 01:55:53 -0500

    first commit

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,35 @@
+class UrlsController < ApplicationController
+
+  def index
+    @urls=Url.all
+  end
+
+  def show
+    url = Url.where(:random_id=>params[:id]).first
+    if url
+      redirect_to url.original
+    else
+      render "index"
+    end
+  end
+
+  def new
+    @url = Url.new
+    letters = [('a'...'z'),('A'...'Z')].map{|i| i.to_a}.flatten
+    @url.random_id=(0...8).map{letters[rand(letters.length)]}.join
+  end
+
+  def url_params
+    params.require(:url).permit(:original,:random_id)
+  end
+
+  def create
+    @url = Url.new(url_params)
+    if @url.save
+      redirect_to urls_path
+    else
+      render "new"
+    end
+  end
+
+end
